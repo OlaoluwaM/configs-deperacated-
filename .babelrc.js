a6c24ec0a682c6f4837c80ef98fd38239e0d8372
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-04-11 17:15:37 -0500

    edits made

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,21 @@
-const isTest = String(process.env.NODE_ENV) === 'test';
+module.exports = api => {
+  const isTest = String(process.env.NODE_ENV) === 'test';
 
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
-    '@babel/preset-react',
-  ],
-  plugins: [
-    'syntax-dynamic-import',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ],
+  return {
+    presets: [
+      ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
+      '@babel/preset-react',
+    ],
+    plugins: [
+      'syntax-dynamic-import',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      [
+        'transform-inline-environment-variables',
+        {
+          include: ['NODE_ENV'],
+        },
+      ],
+    ],
+  };
 };
