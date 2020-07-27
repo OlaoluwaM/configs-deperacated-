0fdad194ede2a9fd8900bf93fb0a16f76fdfc3c9
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-07-26 19:18:54 -0500

    Config update

--- .babelrc.js
+++ .babelrc.js
@@ -1,21 +1,17 @@
-module.exports = api => {
-  const isTest = String(process.env.NODE_ENV) === 'test';
+const isTest = process.env.NODE_ENV === 'test';
 
-  return {
-    presets: [
-      ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
-      '@babel/preset-react',
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      { targets: { node: 'current' }, modules: isTest ? 'cjs' : false },
     ],
-    plugins: [
-      'syntax-dynamic-import',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      [
-        'transform-inline-environment-variables',
-        {
-          include: ['NODE_ENV'],
-        },
-      ],
-    ],
-  };
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'syntax-dynamic-import',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-transform-async-to-generator',
+  ],
 };
---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  ...defaults,
+  testMatch: ['<rootDir>/change-here/__tests__/**/*.test.[j]s?(x)'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/mocks/file.js',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  verbose: true,
+};
